db802ffd5f43b6f23223c81438ae90b6b0243c1e
Harsh Shrivastava <harshsrivas233@gmail.com>
2024-12-25 14:13:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,103 @@
+let users=[
+    {
+      dp:'https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      photo:'https://images.unsplash.com/photo-1734511283694-9b8e3056fb6e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name:'John Doe',
+      
+    },
+    {
+      dp:'https://plus.unsplash.com/premium_photo-1669704098750-7cd22c35422b?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      photo:'https://images.unsplash.com/photo-1734249201319-e7227b1b4f54?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name:'Harsh',
+    },
+    {
+      dp:'https://images.unsplash.com/photo-1616536864877-49b0f53884ed?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      photo:'https://images.unsplash.com/photo-1723926991442-80310b191af8?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name:'Rahul',
+    },
+    {
+      dp:'https://images.unsplash.com/photo-1638993606271-04836e75d662?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      photo:'https://images.unsplash.com/photo-1729762163184-875be1062d8a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name:'Rohan',
+    },
+    {
+      dp:'https://images.unsplash.com/photo-1617632679997-4382a0f0ff78?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      photo:'https://images.unsplash.com/photo-1734490422581-cd3bd481c99e?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name:'Casy',
+    },
+    {
+      dp:'https://images.unsplash.com/photo-1625642123545-f0f68b1621e3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      photo:'https://images.unsplash.com/photo-1732945302017-fbc971324ebe?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name:'Dan',
+    },
+    {
+      dp:'https://images.unsplash.com/photo-1525672892528-eb2eead4effc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      photo:'https://images.unsplash.com/photo-1732878937469-3c1ff28cb338?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name:'Anubhuti',
+    },
+    {
+      dp:'https://images.unsplash.com/photo-1531370114630-ea11cde63962?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      photo:'https://images.unsplash.com/photo-1734523085360-ca37b9ff0cdb?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name:'Mandu',
+    },
+    {
+        dp:'https://plus.unsplash.com/premium_photo-1708110921398-1fc68e98eacc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        photo:'https://images.unsplash.com/photo-1732106450333-5c95a3763d31?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        name:'Dev',
+    },
+    {
+      dp:'https://images.unsplash.com/photo-1566802842272-e694af42eb29?q=80&w=2028&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      photo:'https://images.unsplash.com/photo-1734631083652-6a17bb81d8db?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name:'Trolle'
+    }
+  ]
+
+
+  let sum =''
+
+  users.forEach((elm,idx)=>{
+    sum=sum+` <div  class="story">
+                     <img  id=${idx} src="${elm.dp}" alt="">           
+            </div>
+    
+            `  
+          
+            
+  })
+
+  let storyian=document.querySelector('#storiyan')
+  storyian.innerHTML=sum
+  let full=document.querySelector('#full')
+  let growth=document.querySelector('.growth')
+  let fulluser=document.querySelector('#full h2')
+
+  
+  storyian.addEventListener('click',(elm)=>{
+    let size=0
+    
+     let gold=users[elm.target.id]
+     console.log(gold);
+     
+     full.style.display='block'
+     full.style.backgroundImage=`url(${gold.photo})`
+     fulluser.innerHTML=gold.name
+
+     
+     let interval=setInterval(()=>{
+         size++
+         growth.style.width=size+'%'
+     },30)
+
+
+
+     setTimeout(()=>{
+        clearInterval(interval)
+        full.style.display='none'
+     },3000)
+
+
+       
+  })
+
+//   console.log(sum);
+  
